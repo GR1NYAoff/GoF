a00d852e8c5c2e640596b5c6fd499ae35ddb6ff3
= <=>
2022-05-03 18:14:54 +0300

    add singleton example

---
+++ Singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var s1 = Singleton.GetInstance();
+            var s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+                s1.SomeBusinessLogic();
+                s2.SomeBusinessLogic();
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+
+            return instance;
+        }
+
+        public void SomeBusinessLogic()
+        {
+            Console.WriteLine("Doing singleton logic from {0}", GetHashCode());
+        }
+    }
+
+}
